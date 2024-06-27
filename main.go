1d0eb2525c8379505fe0a2c6040dc9f13b3ac36a
A-Ze <mike504110403@gmail.com>
2024-06-28 02:25:27 +0800

    cookie demo

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	//client "golang_web/httpclientdemo"
-	template "golang_web/templatedemo"
+	//template "golang_web/templatedemo"
+	cookiedemo "golang_web/cookieDemo"
 	"net/http"
 
 	mlog "github.com/mike504110403/goutils/log"
@@ -18,6 +19,7 @@ func Init() {
 func main() {
 	Init()
 	//client.PutDemo()
-	http.HandleFunc("/", template.HelloHandlerFunc)
+	//http.HandleFunc("/", template.HelloHandlerFunc)
+	http.HandleFunc("/", cookiedemo.TestCookieHandler)
 	http.ListenAndServe(":8086", nil)
 }
